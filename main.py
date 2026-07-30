f1f2941c5303a5d2a74226c35be2b74ae763f982
still-spidy <devianks25@gmail.com>
2026-07-30 15:35:37 -0400

    Fix some bugs and better output

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import whois
 
-info = input("Enter Domain Name: ")
+target = input("Enter Domain Name: ")
 print("Fetching Details....")
+
+def normalize(value):
+        if isinstance(value,list):
+            value = value[0]
+        return value
+
 try: 
-    domain_info = whois.whois(info)
-    domain = domain_info.get("domain_name")
-    creation = domain_info.get("creation_date")
-    updation = domain_info.get("updated_date")
-    expiry = domain_info.get("expiration_date")
-    server = domain_info.get("name_servers")
-    email = domain_info.get("emails")
+    domain_info = whois.whois(target)
+    domain = normalize(domain_info.get("domain_name"))
+    creation = normalize(domain_info.get("creation_date"))
+    updation = normalize(domain_info.get("updated_date"))
+    expiry = normalize(domain_info.get("expiration_date"))
+    name_server = domain_info.get("name_servers")
+    emails = domain_info.get("emails")
 
     print("========================\n"
     "WHOIS Lookup Result\n"
     "========================\n"
-    f"Domain Name:   {domain}\n"
-    f"Creation On:   {creation}\n"
-    f"Updated On:   {updation}\n"
-    f"Expired On:   {expiry}\n"
+    f"Domain Name:    {domain}\n"
+    f"Creation On:    {creation}\n"
+    f"Updated On:     {updation}\n"
+    f"Expired On:     {expiry}\n"
     f"Name Servers:")
-    if server:
-       for i in server:
-          print("-",i)
+    if name_server:
+        if isinstance(name_server,list):  
+            for i in name_server:
+                print("-",i)
+        else:
+            print(name_server)
     else:
         print("No Name Servers found.")
 
-    print("Emails: ")
-    if email:
-        for i in email:
-            print("~",i)
+    if emails:
+        print("Emails: ")
+        if isinstance(emails,list):
+            for email in emails:
+                print("~",email)
+        else:
+            print("~",emails)
     else:
         print("No Emails found.")
     
@@ -36,5 +48,5 @@ try:
         value = domain_info.get(field)
         if value:
             print(f"{field.title()}:    {value}")
-except:
-    print("This domain does not exist.")
+except Exception as e:
+    print(f"Error: {e}")
